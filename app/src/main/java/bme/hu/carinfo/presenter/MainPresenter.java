f246984b67a8e0a53ab735335cb145c960501d11
Mobilszoftver laboratórium <mobsoft@aut.bme.hu>
2016-04-11 10:30:35 +0200

    Added presenter classes

---
+++ app/src/main/java/bme/hu/carinfo/presenter/MainPresenter.java
@@ -0,0 +1,25 @@
+package bme.hu.carinfo.presenter;
+
+import javax.inject.Inject;
+
+import hu.bme.aut.amorg.examples.mvpstart.MVPApplication;
+import hu.bme.aut.amorg.examples.mvpstart.interactor.StringInteractor;
+import hu.bme.aut.amorg.examples.mvpstart.view.MainView;
+
+/**
+ * Created by mobsoft on 2016. 04. 11..
+ */
+
+public class MainPresenter extends Presenter<MainView> {
+
+    @Inject
+    public StringInteractor interactor;
+
+    public MainPresenter() {
+        MVPApplication.injector.inject(this);
+    }
+
+    public void doStuff() {
+        view.showString(interactor.getString());
+    }
+}
